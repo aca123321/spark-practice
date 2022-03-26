29afaac1236b0f4a4fa570ba26b9c4be6eef0696
aca123321 <isamudri1@gmail.com>
2022-03-27 02:46:38 +0530

    first working program

--- src/main/java/com/aca123321/Main.java
+++ src/main/java/com/aca123321/Main.java
@@ -1,11 +1,30 @@
 package com.aca123321;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        List<Double> inputData = Arrays.asList(1.0, 2.0, 3.0, 3.0, 4.5);
 
+        SparkConf conf = new SparkConf().setAppName("Basic rdd setup").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
+        JavaRDD<Double> inputRDD = sc.parallelize(inputData);
+
+        JavaRDD<Double> doubledRDD = inputRDD.map((Double a) -> {return a*2.00;});
+        Double sum = doubledRDD.reduce((Double a, Double b) -> {return a+b;});
+
+        System.out.println("Sum of twice of all the input numbers = " + sum);
+
+        sc.close();
     }
 
 }
