5599d882f6120f7e91cef0c603f40886d5610120
aca123321 <isamudri1@gmail.com>
2022-03-28 04:05:17 +0530

    initial commit

--- src/main/java/com/aca123321/Main.java
+++ src/main/java/com/aca123321/Main.java
@@ -1,30 +1,56 @@
 package com.aca123321;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Double> inputData = Arrays.asList(1.0, 2.0, 3.0, 3.0, 4.5);
-
         SparkConf conf = new SparkConf().setAppName("Basic rdd setup").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Double> inputRDD = sc.parallelize(inputData);
+        JavaRDD<String> inputRDD = sc.textFile("src/main/resources/subtitles/input.txt");
 
-        JavaRDD<Double> doubledRDD = inputRDD.map((Double a) -> {return a*2.00;});
-        Double sum = doubledRDD.reduce((Double a, Double b) -> {return a+b;});
+        JavaRDD<String> words = inputRDD.flatMap((String s) -> {
+            ArrayList<String> ret = new ArrayList<>();
+            for(String t: s.replaceAll("[^a-zA-Z\\s]", "").toLowerCase().trim().split(" |,|\\.|-|/|\"|=")) {
+                if(t.length() != 0) {
+                    ret.add(t);
+                }
+            }
+            return ret.iterator();
+        }).filter((String s) -> {
+            return (!Character.isDigit(s.charAt(0))) && (!Util.isBoring(s));
+        });
 
-        System.out.println("Sum of twice of all the input numbers = " + sum);
+        JavaPairRDD<String, Long> wordPairRDD = words.mapToPair((String s) -> {return new Tuple2<String, Long>(s, 1L);});
+        JavaPairRDD<String, Long> wordFreq = wordPairRDD.reduceByKey((Long a, Long b) -> {return a+b;});
+        JavaPairRDD<Long, String> freqWord = wordFreq.mapToPair((Tuple2<String, Long> a) -> {
+            return new Tuple2<>(a._2, a._1);
+        });
+
+        JavaPairRDD<Long, String> result = freqWord.sortByKey(false);
+
+        for(Tuple2<Long, String> a: result.take(10)) {
+            System.out.println(a._1 + ": " + a._2);
+        }
 
         sc.close();
     }
 
+}
+
+class SortByFreq implements Comparator<Tuple2<String, Long>> {
+
+    // Sorting in ascending order of frequency
+    public int compare(Tuple2<String, Long> a, Tuple2<String, Long> b)
+    {
+        return (int)(a._2 - b._2);
+    }
 }
