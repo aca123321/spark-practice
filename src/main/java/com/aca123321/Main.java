9a4cfffe06cf18999c051b21adc4fd6c5620c924
aca123321 <isamudri1@gmail.com>
2022-03-27 17:22:31 +0530

    item frequency through pair RDDs

--- src/main/java/com/aca123321/Main.java
+++ src/main/java/com/aca123321/Main.java
@@ -1,10 +1,13 @@
 package com.aca123321;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,17 +15,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
         List<Double> inputData = Arrays.asList(1.0, 2.0, 3.0, 3.0, 4.5);
 
         SparkConf conf = new SparkConf().setAppName("Basic rdd setup").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<Double> inputRDD = sc.parallelize(inputData);
+        JavaPairRDD<Double, Integer> inputPairs = inputRDD.mapToPair((Double a) -> { return new Tuple2<Double, Integer>(a, 1);});
 
-        JavaRDD<Double> doubledRDD = inputRDD.map((Double a) -> {return a*2.00;});
-        Double sum = doubledRDD.reduce((Double a, Double b) -> {return a+b;});
+        JavaPairRDD<Double, Integer> freqRDD = inputPairs.reduceByKey((Integer a, Integer b) -> {return  a + b;});
 
-        System.out.println("Sum of twice of all the input numbers = " + sum);
+        freqRDD.foreach(val -> {
+            System.out.println("frequency of " + val._1 + ": " + val._2);
+        });
 
         sc.close();
     }
